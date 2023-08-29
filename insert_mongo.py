da6cbbad11b845934b2bb665997e509c318ddc77
Francesco Russo <franci.russo@icloud.com>
2023-08-29 15:41:29 +0200

    insert mongo

--- insert_mongo.py
+++ insert_mongo.py
@@ -18,11 +18,8 @@ Creazione 3 collezioni per distinte
 -1 Calls per identificare le chiamate definite da: id, numero chiamante, numero chiamato, durata
 -2 cells per identificare le celle telefoniche coinvolte nelle chiamate con: id, citta', stato, indirizzo
 -3 people per identificare le persone coinvolte caratterizzate da: numero di telefono, nome, cognome, nome completo
-
 """
 
-
-#
 calls_coll = db["calls"]
 cells_coll= db["cells"]    
 people_coll=db ["people"]
@@ -36,13 +33,10 @@ calls_reader = csv.DictReader(calls_file)
 cells_reader = csv.DictReader(cells_file)
 people_reader = csv.DictReader(people_file)
 
-calls_list = list(dictate(calls_reader))
-cells_list = list(dictate(cells_reader))
-people_list = list(dictate(people_reader))
+calls_list = dictate(calls_reader)
+cells_list = dictate(cells_reader)
+people_list = dictate(people_reader)
 
 insert_calls = calls_coll.insert_many(calls_list)
 insert_cells = cells_coll.insert_many(cells_list)
 insert_people = people_coll.insert_many(people_list)
-
-
-    
