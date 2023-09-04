9b648f4fb2644c042a2a1380c19feb08cf5d6a34
Francesco Russo <franci.russo@icloud.com>
2023-09-04 23:44:36 +0200

    inserimento di mongo_query

--- mongo_query.py
+++ mongo_query.py
@@ -1,15 +1,22 @@
 import pymongo
+import datetime
 
 
 percentage = [25, 50, 75, 100]
 
-result_csv = open("result.csv", "w")
+result_csv = open("csv/result.csv", "w")
 
 client = pymongo.MongoClient("localhost", 27017)
 
-query = []
+start_search = 1672617600
+end_search = 1672703999
+
+query = [{"$match": {"StartDate": {"$gte": start_search,
+                                      "$lt": end_search}}}]
 
 for p in percentage:
     call = client["progetto"+str(p)]["call"]
-    if call:
-        print("si")
+    start_time = datetime.datetime.now()
+    call.find(query[0])
+    end_time = datetime.datetime.now() - start_time
+    print(end_time)
