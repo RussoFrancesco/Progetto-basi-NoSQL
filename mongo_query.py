8cf65ace0ed144db6ef0e017d763dde6ece64646
Francesco Russo <franci.russo@icloud.com>
2023-09-05 12:22:02 +0200

    Co-authored-by: DOMENICO VILLARI 527221 <domenico.villari1@studenti.unime.it>

--- mongo_query.py
+++ mongo_query.py
@@ -1,10 +1,16 @@
 import pymongo
-import datetime
+import time
+import csv
 
+def clear_cache(database):
+    database.command("planCacheClear", "calls")
+    database.command("planCacheClear", "cells")
+    database.command("planCacheClear", "people")
 
 percentage = [25, 50, 75, 100]
 
-result_csv = open("csv/result.csv", "w")
+result_csv = open("csv/result.csv", "w", newline='\n')
+writer_result = csv.writer(result_csv)
 
 client = pymongo.MongoClient("localhost", 27017)
 
@@ -12,11 +18,29 @@ start_search = 1672617600
 end_search = 1672703999
 
 query = [{"$match": {"StartDate": {"$gte": start_search,
-                                      "$lt": end_search}}}]
+                                      "$lt": end_search}}},
 
-for p in percentage:
-    call = client["progetto"+str(p)]["call"]
-    start_time = datetime.datetime.now()
-    call.find(query[0])
-    end_time = datetime.datetime.now() - start_time
-    print(end_time)
+        {"$match":{}}                              
+                                      ]
+
+
+for j in range(1, len(query)+1):
+    for p in percentage:
+        database = client["progetto"+str(p)]
+        call = database["call"]    #mi connetto ad il singolo db ed alla collezione calls di ognuno di questo 
+        header = ["mongo"+str(p)+"_query"+str(j)]
+        writer_result.writerow(header)    
+        
+
+        results= []
+        
+        for i in range(31):
+            print(i)
+            start_time = time.time_ns() #tempo iniziale in nanosecondi per evitare perdite di approssimazione      
+            call.find(query[j-1]) #esecuzione della query 
+            end_time = (time.time_ns() - start_time) / 10000000 #calcolo del tempo di esecuzione in millisecondi (divisione per 1 milione)
+            results.append(end_time)
+        print(results)
+        writer_result.writerow(results)   
+        clear_cache(database)    
+
